56b87d5e8f8832365b1f1fa07949a56bb2b53e43
nfrydenholm <nfrydenholm@gmail.com>
2019-10-31 13:18:51 +0100

    Update xchtmlreport.rb

--- xchtmlreport.rb
+++ xchtmlreport.rb
@@ -3,7 +3,7 @@ class Xchtmlreport < Formula
   homepage "https://github.com/TitouanVanBelle/XCTestHTMLReport"
   url "https://github.com/TitouanVanBelle/XCTestHTMLReport/archive/1.7.0.tar.gz"
   sha256 "85ac4fd6110e9919006e3ae8a66e6ff31917495312435d0faa49d4325cbb1170"
-  head "https://github.com/TitouanVanBelle/XCTestHTMLReport.git", :branch => "develop", :revision => "e814d1"
+  head "https://github.com/TitouanVanBelle/XCTestHTMLReport.git", :branch => "develop", :revision => "9a04357"
 
   def install
     system "xcodebuild clean build CODE_SIGN_IDENTITY=\"\" CODE_SIGNING_REQUIRED=NO -workspace XCTestHTMLReport.xcworkspace -scheme XCTestHTMLReport -configuration Release"
